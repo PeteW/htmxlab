e81593a64af28e9e47648c054a3049751eaaf880
INOVALON\Peter.Weissbrod <peter.weissbrod@inovalon.com>
2023-01-09 08:12:57 -0500

    Added TODO sample app

---
+++ E2/Controllers/TodoController.cs
@@ -0,0 +1,119 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public TodoController() { Db.EnsureDatabase(); }
+        public IActionResult Index() => View();
+        public async Task<IActionResult> List(TodoStatus? status) => PartialView(Db.GetByStatus(status));
+        public async Task<IActionResult> Edit(int id = 0) => PartialView(id == 0 ? new TodoViewModel() : Db.GetById(id));
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Db.DeleteById(id);
+            Response.Headers.Add("HX-Trigger-After-Swap", "itemSaved");
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(TodoViewModel todoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await Db.InsertOrUpdateTodo(todoViewModel);
+                // HTMX looks for headers like this in the response. they become programmable JS event hooks
+                // https://htmx.org/headers/hx-trigger
+                Response.Headers.Add("HX-Trigger-After-Swap", "itemSaved");
+            }
+            
+            return PartialView(todoViewModel);
+        }
+    }
+
+    public enum TodoStatus { New, Active, Done, }
+    
+    public class TodoViewModel : IValidatableObject
+    {
+        public int Id { get; set; }
+        public TodoStatus Status { get; set; }
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Demo of custom server-side validation logic
+        /// </summary>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(Description))
+            {
+                return new[] {new ValidationResult("You need to fill out the description field", new[] {nameof(Description)})};
+            }
+            else if (!Description.ToLower().StartsWith("please"))
+            {
+                return new[] {new ValidationResult("Todo must start with 'please'", new[] {nameof(Description)})};
+            }
+            else if (Description.Length > 10)
+            {
+                return new[] {new ValidationResult("Todo max length is 10", new[] {nameof(Description)})};
+            }
+            else if (Db.IsDescriptionInUse(Description))
+            {
+                return new[] {new ValidationResult("This description is a duplicate", new[] {nameof(Description)})};
+            }
+            else
+            {
+                return Array.Empty<ValidationResult>();
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Let's pretend this is an interface to some database
+    /// </summary>
+    public class Db
+    {
+        private static List<TodoViewModel> _db;
+        public static async Task EnsureDatabase()
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(250));
+            if (_db == null)
+            {
+                _db = new List<TodoViewModel>();
+                for (var i = 1; i < 5; i++)
+                {
+                    _db.Add(new TodoViewModel
+                    {
+                        Id = i,
+                        Description = $"Please do item #{i}",
+                        Status = Enum.GetValues<TodoStatus>()[i % Enum.GetValues<TodoStatus>().Length],
+                    });
+                }
+            }
+        }
+
+        public static TodoViewModel GetById(int id) => _db.Single(x => x.Id == id);
+
+        public static bool IsDescriptionInUse(string description) =>
+        _db.Any(x => x.Description.Equals(description, StringComparison.OrdinalIgnoreCase));
+        
+        public static List<TodoViewModel> GetByStatus(TodoStatus? status) =>
+        _db.Where(x => !status.HasValue || x.Status == status).ToList();
+
+        public static void DeleteById(int id) => _db = _db.Where(x => x.Id != id).ToList();
+        
+        public static async Task InsertOrUpdateTodo(TodoViewModel todoViewModel)
+        {
+            await EnsureDatabase();
+            if (todoViewModel.Id == 0)
+            {
+                todoViewModel.Id = _db.Max(x => x.Id) + 1;
+                _db.Add(todoViewModel);
+            }
+
+            _db.Single(x => x.Id == todoViewModel.Id).Description = todoViewModel.Description;
+            _db.Single(x => x.Id == todoViewModel.Id).Status = todoViewModel.Status;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,7 +12,9 @@ HTMX poses a significant disruption to front-end development for many business u
 The key benefit is in the code you DONT write:
 
  - Remove the need for a separate/independent "web tier" application layer including the need to host and secure web-tier assets.
+ - Remove the need for "environment awareness" and maintenance of frontend configuration.
  - Remove the need for javascript package managers and downloading hundreds of javascript packages to build a front-end application.
+ - Remove the need for complicated automated build processes to compile front-end application.
  - Remove duplicative data-transfer-objects on both client-side and server-side.
  - Remove duplicative authorization checks on client-side and server-side.
  - Remove duplicative form validation logic on client-side and server-side.
@@ -20,11 +22,12 @@ The key benefit is in the code you DONT write:
  - Remove the need for complex client-side state-management and return to managing state in DOM.
  - Case studies [indicate 66% codebase reduction](https://htmx.org/essays/a-real-world-react-to-htmx-port/) for non-trivial apps.
 
-HTMX is more than just code reduction. There's noteworthy benefits this paradigm shift offers above and beyond SPAs:
+But HTMX is more than just code reduction. There's noteworthy benefits this paradigm shift offers above and beyond SPAs:
  - Through use of partial-sharing, support for sharing UI "components" between adjacent apps and microservices becomes trivial. Interchangable partials drastically simplifies the logistics of application UI sharing, integrations and microservice UIs.
  - Reactjs applications can reach or exceed 1MB in size. HTMX comes as a single 11kb file thus improving load time.
  - Remove the need for virtual doms required by reactjs thus improving performance on client-side and allowing larger page content. The end result is a snappier UI performance.
  - Jquery and MVC-razor technologies are over a decade old. Finding competent teammates capable of developing server-side is more feasible. Development among smaller teams with less tiers takes significantly less time and people.
+ - Less cognitive baggage for "full stack" engineering teams. To 
  - Hypermedia guides developers to use more intuitive RESTful structures which includes data and methods. Single-page-apps tend to read-write JSON which has a tendency to lean towards RPC. For more information on this point see [this essay regarding HATEOAS](https://htmx.org/essays/hateoas/).
 
 ## Caveats
